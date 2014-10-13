47b7c49922f227f738a1c74c26433a9beeae586a
Jaume991 <jaume.riera.1234@gmail.com>
2014-10-13 09:51:10 +0200

    [Backend] - Seeds dels recursos restants

---
+++ app/database/seeds/ActivitiesTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use DawWiki\Activities\Activity;
+use DawWiki\Topics\Topic;
+use Faker\Factory as Faker;
+
+class ActivitiesTableSeeder extends Seeder {
+
+	public function run()
+	{
+		$faker = Faker::create();
+		$topics = Topic::lists('id');
+
+		foreach(range(1, 10) as $index)
+		{
+			Activity::create([
+				'topic_id' => $faker->randomElement($topics),
+				'title' => 'Activitat' . $index,
+				'statement' => $faker->sentence(6)
+			]);
+		}
+	}
+
+}
---
+++ app/database/seeds/TopicsTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use DawWiki\Subjects\Subject;
+use DawWiki\Topics\Topic;
+use Faker\Factory as Faker;
+
+class TopicsTableSeeder extends Seeder {
+
+	public function run()
+	{
+		$faker = Faker::create();
+		$subjects =  Subject::lists('id');
+
+		foreach(range(1, 10) as $index)
+		{
+			Topic::create([
+				'subject_id' => $faker->randomElement($subjects),
+				'name' => 'Tema' . $index
+			]);
+		}
+	}
+
+}
