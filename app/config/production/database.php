8e7e6894794a59f9b7a193fb32f5e8b7288372ed
xavitb3 <xavitb3@gmail.com>
2014-10-17 14:34:21 +0200

    [Backend] - Typo al nom de la variable d'entorn de la config de la bbdd

--- app/config/production/database.php
+++ app/config/production/database.php
@@ -9,7 +9,7 @@ return array(
         'mysql' => array(
             'driver'    => 'mysql',
             'host'      => getenv('DB_HOST'),
-            'database'  => getenv('DB_DATABASE'),
+            'database'  => getenv('DB_NAME'),
             'username'  => getenv('DB_USER'),
             'password'  => getenv('DB_PASSWORD'),
             'charset'   => 'utf8',
