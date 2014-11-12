456c1f211ccb328e5a7450c592f8145c29d6f00d
xavitb3 <xavitb3@gmail.com>
2014-11-12 09:46:46 +0100

    [Backend] - Tabulació sobrant

--- app/config/twitter.php
+++ app/config/twitter.php
@@ -4,6 +4,6 @@ return [
 
 	'consumer_key'    => getenv('twitter_consumer_key'),
  	'consumer_secret' => getenv('twitter_consumer_secret'),
-	'token'			  => getenv('twitter_token'),
+	'token'			      => getenv('twitter_token'),
 	'token_secret'    => getenv('twitter_token_secret')
 ];
